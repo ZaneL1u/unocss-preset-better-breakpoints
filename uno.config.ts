afe2249963f453b3cb22d1b6165f434d9318bc81
zaneliu <hi@zaneliu.me>
2025-03-01 18:28:02 +0800

    fix: remove presetUno

--- uno.config.ts
+++ uno.config.ts
@@ -1,11 +1,9 @@
-import { defineConfig, presetUno } from 'unocss'
+import { defineConfig } from 'unocss'
 import { presetBetterBreakpoints } from './src'
 
 // Just for Vscode Extension
-
 export default defineConfig({
   presets: [
-    presetUno(),
     presetBetterBreakpoints({
       span: 24,
     }),
